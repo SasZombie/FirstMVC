5568a2418987a5bc8321a17a3df4f285ccc29e44
bimbimbambam <bimbambambim@proton.me>
2024-05-28 17:24:44 +0300

    we are coocked

--- src/main/java/com/wad/firstmvc/FirstMvcApplication.java
+++ src/main/java/com/wad/firstmvc/FirstMvcApplication.java
@@ -2,7 +2,7 @@ package com.wad.firstmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//we are so coocked
 @SpringBootApplication
 public class FirstMvcApplication {
     public static void main(String[] args) {
